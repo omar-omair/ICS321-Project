6abc63e4d7ab8db4fac1828fe2b74007c985bca3
Omar <omaromair123@gmail.com>
2024-05-07 16:52:44 +0300

    started front end

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
