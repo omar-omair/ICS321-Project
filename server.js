72bbac9248e98468d3b29c948d634f7e7cce5166
Omar <omaromair123@gmail.com>
2024-05-07 16:38:58 +0300

    initializing node server

---
+++ server.js
@@ -0,0 +1,33 @@
+const postgres = require('postgres');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+PGUSER = decodeURIComponent(PGUSER);
+PGDATABASE = decodeURIComponent(PGDATABASE);
+
+const sql = postgres({
+    host: PGHOST,
+    database: PGDATABASE,
+    username: PGUSER,
+    password: PGPASSWORD,
+    port: 5432,
+    ssl: 'require',
+    connection: {
+        options: `project=${ENDPOINT_ID}`,
+    },
+});
+
