162abc843cb4c98bb90f6d7c1899877fe5c94a8c
Omar <omaromair123@gmail.com>
2024-05-13 00:51:57 +0300

    login function created

---
+++ public/frontendjs/login.js
@@ -0,0 +1,38 @@
+window.onload = main()
+
+function main() {
+    let login_button = document.getElementById('login_button')
+    login_button.addEventListener('click', async function (e) {
+        e.preventDefault()
+        console.log("gg")
+        let emailInput = document.getElementById('email')
+        let passwordInput = document.getElementById('password')
+        let test_email = emailInput.value
+        let test_password = passwordInput.value
+
+
+        await fetch('http://localhost:3000/login/accounts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: test_email,
+                password: test_password
+            })
+        }).then(response => {
+            if (!response.ok) {
+                console.log("emaiiaiaialk not fdound error")
+                throw new Error("")
+            }
+        }).then(data => {
+            localStorage.setItem('user', JSON.stringify(data));
+
+            window.location.href = '/booking';
+
+        }).catch(error => {
+            alert(error.message);
+        });
+
+    })
+}
--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const db = require('./db.js');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 app.use(express.static("public"));
+
+app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
     try {
@@ -25,6 +29,40 @@ app.get('/', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
+app.get("/login", function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'log.html'));
+})
+
+app.get("/booking", function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'booking.html'));
+})
+
+app.post("/login/accounts", async function (req, res) {
+    try {
+        const { email, password } = req.body;
+        const response = await new Promise((resolve, reject) => {
+            db.query("SELECT email, password FROM passenger", (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result.rows);
+                }
+            });
+        });
+
+        const user = response.find(element => email === element.email && password === element.password);
+        if (user) {
+            res.status(200).send("OK"); // Send user data back if found
+        } else {
+            res.status(404).send('Not Found'); // Send 404 if user not found
+        }
+    } catch (error) {
+        console.error('Error executing query:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
 
 
 app.listen(PORT, () => {
