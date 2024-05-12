8cf35572ab571ed0834195409d96456b50dcfcb6
Omar <omaromair123@gmail.com>
2024-05-12 23:41:52 +0300

    accessing data

--- public/frontendjs/test.js
+++ public/frontendjs/test.js
@@ -4,6 +4,8 @@ async function main() {
     let p = [];
     await fetch("http://localhost:3000/").then(response => response.json()).then(data => { p = data; });
     console.log(p);
-    document.querySelector(".container").innerHTML += `<p> ${p[0]} </p>`
+    fid = []
+    p.forEach(t => (fid.push(t)))
+    document.querySelector(".container").innerHTML += `<p> ${fid.toString()} </p>`
 }
 
